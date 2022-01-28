e697fd54b24b6658175010fd2960924c4b4d41fd
Jonathan Experton <jonathan.experton@gmail.com>
2022-01-28 15:38:04 -0500

    Add random name

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { getRandomName } = require("./fn/randomName");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send({ COMMIT_SHORT_HASH: process.env.COMMIT_SHORT_HASH });
+  res.send({
+    commitHash: process.env.COMMIT_SHORT_HASH,
+    randomName: getRandomName(),
+  });
 });
 
 app.listen(port, () => {
